1c1d35072c6aa1ff9c65d5f75586cbba4cb4b32c
Amandeep98781 <77733148+Amandeep98781@users.noreply.github.com>
2021-01-28 14:47:10 -0500

    2nd commit

--- note_ASR_ios/ViewController.swift
+++ note_ASR_ios/ViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
